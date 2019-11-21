b7eecaa0a2891f4d12e152573f6fa9c727af2f15
岸本陽大 <youodf11khp@gmail.com>
2019-11-21 21:30:59 +0900

    145 ~ C

---
+++ abc/145/B_Echo.py
@@ -0,0 +1,12 @@
+N = int(input())
+S = input()
+
+if N % 2 != 0:
+  print("No")
+
+else:
+  if S[:int(len(S)/2)] == S[int(len(S)/2):]:
+    print("Yes")
+  else:
+    print("No")
+
---
+++ abc/145/C_AverageLength.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy
+from scipy.spatial import distance_matrix
+
+N = int(input())
+
+cords = []
+for _ in range(N):
+  sp = input().split()
+  x, y = float(sp[0]), float(sp[1])
+  cords.append([x, y])
+
+cords = np.array(cords)
+d = distance_matrix(cords, cords)
+np.fill_diagonal(d, 0)
+print(d.sum()/cords.size*2)
