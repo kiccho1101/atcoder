df48bec311099cc4e9c839e6dca053fd56c94d3f
yodai.a.kishimoto <yodai.a.kishimoto@rakuten.com>
2022-02-27 19:37:54 +0900

    add abc085

---
+++ abc051/b/main.py
@@ -0,0 +1,12 @@
+K, S = map(int, input().split())
+
+ans = 0
+for x in range(K + 1):
+    for y in range(K + 1):
+        z = S - x - y
+        if 0 <= z <= K:
+            ans += 1
+        if z < 0:
+            break
+
+print(ans)
